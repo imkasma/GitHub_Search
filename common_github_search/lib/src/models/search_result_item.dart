3e066a4d9bc22a0ecf5d958a62214434d72cbfbe
palupiazalia <lupiktp@gmail.com>
2026-05-15 23:14:52 +0700

    search result item

--- common_github_search/lib/src/models/search_result_item.dart
+++ common_github_search/lib/src/models/search_result_item.dart
@@ -1,21 +1,37 @@
-import 'package:common_github_search/src/models/github_user.dart';
+import 'package:flutter/material.dart';
+import 'models/search_result_item.dart';
 
-class SearchResultItem {
-  const SearchResultItem({
-    required this.fullName,
-    required this.htmlUrl,
-    required this.owner,
-  });
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key, required this.items});
 
-  factory SearchResultItem.fromJson(Map<String, dynamic> json) {
-    return SearchResultItem(
-      fullName: json['full_name'] as String,
-      htmlUrl: json['html_url'] as String,
-      owner: GithubUser.fromJson(json['owner'] as Map<String, dynamic>),
+  final List<SearchResultItem> items;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Search Result')),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(item.owner.avatarUrl),
+            ),
+            title: Text(
+              item.fullName,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(item.owner.login),
+            trailing: const Icon(Icons.open_in_new),
+            onTap: () {
+              // sementara hanya print url
+              debugPrint(item.htmlUrl);
+            },
+          );
+        },
+      ),
     );
   }
-
-  final String fullName;
-  final String htmlUrl;
-  final GithubUser owner;
 }
